c0cb88f662228a3b86e6bf3618abd0ea75fcbadf
Hendrik Herpers <hherpers@network-publishing.de>
2022-10-07 17:32:01 +0200

    [@qcandymanp] basic setup for installation

---
+++ ext_emconf.php
@@ -0,0 +1,16 @@
+<?php
+$EM_CONF[$_EXTKEY] = [
+	'title' => 'sweet-imagemap',
+	'description' => 'A TYPO3 extension to create imagemaps.',
+	'category' => 'be',
+	'author' => 'Hendrik Herpers',
+	'author_company' => 'network.publishing GmbH',
+	'state' => 'alpha',
+	'clearCacheOnLoad' => 1,
+	'version' => '0.0.1',
+	'constraints' => [
+		'depends' => [
+			'typo3' => '10.4.0-11.99.99',
+		],
+	],
+];
